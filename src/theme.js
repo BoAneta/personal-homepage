78044072dd78037b0895ec03148e94981335f7a0
Aneta Borowski <borowski.aneta@gmail.com>
2023-03-13 13:58:11 +0100

    colors added to theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,11 @@
 export const theme = {
     color: {
+        white: "#FFFFFF",
+        ironTransparent: "rgba(209, 213, 218, 0.3)",
         mercury: "#E5E5E5",
+        slategray: "#6E7E91",
+        scienceblue: "#0366D6",
+        violet: "#090A33",
         mineshaft: "#252525",
     },
 };
