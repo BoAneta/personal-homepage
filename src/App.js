cad84e8042a5e9207571c1de28182c6da4d76b2a
Aneta Borowski <borowski.aneta@gmail.com>
2023-03-21 19:50:18 +0100

    PersonalHompage js created and App component updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import { Container } from "./common/Container/styled";
-import { Footer } from "./features/PersonalHomepage/Footer";
-import { Header } from "./features/PersonalHomepage/Header";
-import { ModeSwitcher } from "./common/ModeSwitcher";
-import { Portfolio } from "./features/PersonalHomepage/Portfolio";
-import { Skills } from "./features/PersonalHomepage/Skills";
-import { skills, toLearn } from "./features/PersonalHomepage/Skills/skillsList";
+import { ThemeProvider } from "styled-components";
+import PersonalHomepage from "./features/PersonalHomepage";
+import { GlobalStyle } from "./common/GlobalStyle";
+import { lightMode } from "./common/ModeSwitcher/theme";
 
 function App() {
   return (
-    <Container>
-      <ModeSwitcher />
-      <Header />
-      <Skills
-        title="My skillset includes 🛠️"
-        skills={skills}
-      />
-      <Skills
-        title="What I want to learn next 🚀"
-        skills={toLearn} />
-      <Portfolio
-        title="Portfolio" 
-        subTitle="My recent projects"
-      />
-      <Footer />
-    </Container>
+    <ThemeProvider theme={lightMode}>
+      <GlobalStyle />
+      <PersonalHomepage />
+    </ThemeProvider>
   );
 }
 
---
+++ src/features/PersonalHomepage/index.js
@@ -0,0 +1,30 @@
+import { Container } from "../../common/Container/styled";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { ModeSwitcher } from "../../common/ModeSwitcher";
+import { Portfolio } from "./Portfolio";
+import { Skills } from "./Skills";
+import { skills, toLearn } from "./Skills/skillsList";
+
+const PersonalHomepage = () => {
+  return (
+    <Container>
+      <ModeSwitcher />
+      <Header />
+      <Skills
+        title="My skillset includes 🛠️"
+        skills={skills}
+      />
+      <Skills
+        title="What I want to learn next 🚀"
+        skills={toLearn} />
+      <Portfolio
+        title="Portfolio" 
+        subTitle="My recent projects"
+      />
+      <Footer />
+    </Container>
+  );
+}
+
+export default PersonalHomepage;
