e9dcb2ac0197265b52c97bdbcaebc90711a164b0
Aneta Borowski <borowski.aneta@gmail.com>
2023-04-06 14:01:55 +0200

    ButtonLink fixed

--- src/features/PersonalHomepage/PersonalHomepage/ButtonLink/styled.js
+++ src/features/PersonalHomepage/PersonalHomepage/ButtonLink/styled.js
@@ -3,18 +3,13 @@ import styled from "styled-components";
 export const ButtonLink = styled.button`
     border: 1px solid ${({ theme }) => theme.color.buttonLink.border};
     font-size: 20px;
-    letter-spacing: 0.05em;
     background-color: ${({ theme }) => theme.color.primary};
     color: ${({ theme }) => theme.color.buttonLink.text};
     font-size: 20px;
     font-weight: 600;
-    margin: 32px 0;
     padding: 12px 16px;
-    border-radius: 4px;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    transition: 1s;
+    border-radius: ${({ theme }) => theme.borderRadius};
+    transition: box-shadow 0.3s;
 
     &:hover {
         box-shadow: 0 0 2px 2px
@@ -24,5 +19,9 @@ export const ButtonLink = styled.button`
     &:active {
         box-shadow: 0 -2px 0 0
         ${({ theme }) => theme.color.buttonLink.active};
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 18px;
     }
 `;
