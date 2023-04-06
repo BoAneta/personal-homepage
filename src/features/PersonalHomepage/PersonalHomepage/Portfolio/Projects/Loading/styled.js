69f5e854e6f9f04859125029e1ea87c38dcf5cab
Aneta Borowski <borowski.aneta@gmail.com>
2023-04-06 13:44:08 +0200

    media Queries added to Loading component

--- src/features/PersonalHomepage/PersonalHomepage/Portfolio/Projects/Loading/styled.js
+++ src/features/PersonalHomepage/PersonalHomepage/Portfolio/Projects/Loading/styled.js
@@ -14,9 +14,19 @@ export const Wrapper = styled.div`
     color: ${({ theme }) => theme.color.textPrimary};
     display: grid;
     justify-items: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 17px;
+        margin-top: 32px;
+    }
 `;
 
 export const Icon = styled(Spinner)`
     margin-top: 48px;
     animation: ${rotate} 1s infinite linear;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        width: 60px;
+        margin-top: 32px;
+    }
 `;
