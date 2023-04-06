a547db1929ba48590073a15062f20e7abae7c6d6
Aneta Borowski <borowski.aneta@gmail.com>
2023-04-06 14:13:50 +0200

    footer styled fixed

--- src/features/PersonalHomepage/PersonalHomepage/Footer/styled.js
+++ src/features/PersonalHomepage/PersonalHomepage/Footer/styled.js
@@ -1,18 +1,10 @@
 import styled from "styled-components";
-import { Content } from "../Header/styled";
-import { Title } from "../Portfolio/styled";
 
-
-export const Container = styled.div`
-    max-width: 670px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    gap: 24px;
-    margin-left: 62px;
+export const Wrapper = styled.footer`
+    margin-top: 120px;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}px) {
-        margin: 0;
+        margin-top: 48px;
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
@@ -20,49 +12,49 @@ export const Container = styled.div`
     }
 `;
 
-export const Mail = styled.a`
-    text-decoration: none;
+export const LetsTalk = styled.h2`
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: initial;
+    margin: 0;
 `;
 
-export const Contact = styled(Title)`
-    transition: 1s;
+export const Address = styled.address`
+    font-style: unset;
+`;
+
+export const EmailWrapper = styled.div`
+    margin: 24px 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin: 12px 0;
+    }
+`;
+
+export const Contact = styled.a`
+    color: ${({ theme }) => theme.color.textPrimary};
+    font-weight: 900;
+    font-size: 32px;
+    text-decoration: none;
+    transition: color 0.3s;
 
     &:hover {
         color: ${({ theme }) => theme.color.primary};
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 20px;
+        font-size: 18px;
     }
 `;
 
-export const FooterContent = styled(Content)`
+export const FooterContent = styled.p`
+    max-width: 670px;
+    font-size: 18px;
+    line-height: 1.4;
+    margin: 0;
     color: ${({ theme }) => theme.color.textPrimary};
-`;
-
-export const Icons = styled.div`
-    display: flex;
-    gap: 24px;
-    margin-bottom: 110px;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}px) {
-        margin-bottom: 30px;
-    }
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        margin-bottom: 30px;
-    }
-`;
-
-export const SocialLink = styled.a`
-    svg path {
-            transition: 0.3s;
-            fill: ${({ theme }) => theme.color.socialIcons.color};
-    }
-    
-      &:hover {
-        svg path {
-            fill: ${({ theme }) => theme.color.socialIcons.hover};
-        }    
+        font-size: 14px;
     }
 `;
